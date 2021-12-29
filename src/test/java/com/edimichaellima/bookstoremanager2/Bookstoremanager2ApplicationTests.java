520e571c821969b8c86b8259ea405a66ced4efa1
Edimichael Lima <edimichael.lima@gmail.com>
2021-12-29 17:20:47 -0300

    Initial commit

---
+++ src/test/java/com/edimichaellima/bookstoremanager2/Bookstoremanager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edimichaellima.bookstoremanager2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstoremanager2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
